46201a972a72b3fdf93850e27236732dcf2e328f
stefanc <stefan@themeisle.com>
2017-09-12 12:08:39 +0300

    #5 Add notification in dashboard and customizer
    Notification that the theme is not maintained anymore added in customizer and dashboard
    
    The following tags were removed because they are deprecated:
    white, responsive-layout, gray, blue

--- functions.php
+++ functions.php
@@ -27,3 +27,51 @@ if ( ! function_exists( 'flat_sky_setup' ) ) :
 	}
 endif;
 add_action( 'after_setup_theme', 'flat_sky_setup' );
+
+
+/**
+ * Notice in Customize to announce the theme is not maintained anymore
+ */
+function flat_sky_customize_register( $wp_customize ) {
+
+	require_once get_stylesheet_directory() . '/class-ti-notify.php';
+
+	$wp_customize->register_section_type( 'Ti_Notify' );
+
+	$wp_customize->add_section(
+		new Ti_Notify(
+			$wp_customize,
+			'ti-notify',
+			array( /* translators: Link to the recommended theme */
+				'text'     => sprintf( __( 'This theme is not maintained anymore, check-out our latest free one-page theme: %1$s.','flat-sky' ), sprintf( '<a href="' . admin_url( 'theme-install.php?theme=hestia' ) . '">%s</a>', 'Hestia' ) ),
+				'priority' => 0,
+			)
+		)
+	);
+
+	$wp_customize->add_setting( 'flat-sky-notify', array(
+		'sanitize_callback' => 'esc_html',
+	) );
+
+	$wp_customize->add_control( 'flat-sky-notify', array(
+		'label'    => __( 'Notification', 'flat-sky' ),
+		'section'  => 'ti-notify',
+		'priority' => 1,
+	) );
+}
+add_action( 'customize_register', 'flat_sky_customize_register' );
+
+/**
+ * Notice in admin dashboard to announce the theme is not maintained anymore
+ */
+function flat_sky_admin_notice() {
+
+	global $pagenow;
+
+	if ( is_admin() && ( 'themes.php' == $pagenow ) && isset( $_GET['activated'] ) ) {
+		echo '<div class="updated notice is-dismissible"><p>';
+		printf( /* translators: link to the recommended theme */ __( 'This theme is not maintained anymore, check-out our latest free one-page theme: %1$s.','flat-sky' ), sprintf( '<a href="' . admin_url( 'theme-install.php?theme=hestia' ) . '">%s</a>', 'Hestia' ) );
+		echo '</p></div>';
+	}
+}
+add_action( 'admin_notices', 'flat_sky_admin_notice', 99 );
